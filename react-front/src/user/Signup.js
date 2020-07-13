c7c9a237ad498302620851e9d7c5f0c94b085f2a
Yash Gupta <yash04g@gmail.com>
2020-07-13 20:41:30 +0530

    Started with React

--- react-front/src/user/Signup.js
+++ react-front/src/user/Signup.js
@@ -1,12 +1,40 @@
 import React from 'react';
 
 class Signup extends React.Component{
+    constructor(){
+        super();
+        this.state = {
+            name : "",
+            email : "",
+            password : "",
+            error: ""
+        }
+    }
     render(){
-        return(
-            <div>
-                <h2>Signup</h2>
-            </div>
-        )
+        return (
+          <div className="container">
+            <h2 className="mt-5 mb-5">Signup</h2>
+            <form>
+              <div className="form-group">
+                <label className="text-muted">Name</label>
+                <input type="text" className="form-control" />
+              </div>
+            </form>
+            <form>
+              <div className="form-group">
+                <label className="text-muted">Email</label>
+                <input type="email" className="form-control" />
+              </div>
+            </form>
+            <form>
+              <div className="form-group">
+                <label className="text-muted">Name</label>
+                <input type="password" className="form-control" />
+              </div>
+            </form>
+            <button className="btn btn-raised btn-primary">Submit</button>
+          </div>
+        );
     }
 }
 
